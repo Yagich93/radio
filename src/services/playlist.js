4e2a84114545a894c3e100ba0db0587cd6a3ac6a
Iakov Lushin <iakov.lushin@fulldive.com>
2019-12-07 14:36:09 +0300

    Add logic of generating playlist by last letter

--- src/services/playlist.js
+++ src/services/playlist.js
@@ -1,6 +1,22 @@
+const _ = require('lodash')
+
 class Playlist {
+  constructor(songDb) {
+    this.songDb = songDb
+  }
+
   generate(n) {
-    return new Array(n)
+    const firstSong = this.songDb.sample()
+    const songs = [firstSong]
+    _.times(n - 1, () => this._appendNextSong(songs))
+    return songs
+  }
+
+  _appendNextSong(songs) {
+    const lastSong = songs[songs.length - 1]
+    const [firstLetter] = lastSong.title
+    const nextSong = this.songDb.sample({ firstLetter })
+    songs.push(nextSong)
   }
 }
 
