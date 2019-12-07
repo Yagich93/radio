3eda9ac2046c876e88d6fdb9da6bed5bb33e261c
Iakov Lushin <iakov.lushin@fulldive.com>
2019-12-07 16:24:18 +0300

    Add test of handling not found song

--- test/lib/song-db-mock.js
+++ test/lib/song-db-mock.js
@@ -1,12 +1,33 @@
-const songDbMock = {
-  sample: ({ firstLetter = 'y' } = {}) => {
-    const titleMap = {
-      y: 'YEAH RIGHT',
-      T: 'Toxic',
-      c: 'Chop Suey'
+const INFINITE_TITLE_MAP = {
+  y: 'YEAH RIGHT',
+  T: 'Toxic',
+  c: 'Chop Suey'
+}
+
+const TITLE_MAP_WITH_NOT_FOUND_LETTERS = {
+  y: 'YEAH RIGHT!!!',
+  T: 'Toxic Poison',
+  c: 'Chop Suey'
+}
+
+class SongDbMock {
+  constructor() {
+    this.titleMap = INFINITE_TITLE_MAP
+  }
+  sample({ firstLetter = 'y' } = {}) {
+    const title = this.titleMap[firstLetter]
+    return title ? { title } : null
+  }
+
+  _setMode(mode) {
+    switch (mode) {
+      case 'withNotFoundLetters':
+        this.titleMap = TITLE_MAP_WITH_NOT_FOUND_LETTERS
+        return
+      default:
+        this.titleMap = INFINITE_TITLE_MAP
     }
-    return { title: titleMap[firstLetter] }
   }
 }
 
-module.exports = { songDbMock }
+module.exports = { SongDbMock }
--- test/services/playlist.js
+++ test/services/playlist.js
@@ -2,11 +2,12 @@ const _ = require('lodash')
 const chai = require('chai')
 const chaiString = require('chai-string')
 const { Playlist } = require('../../src/services/playlist')
-const { songDbMock } = require('../lib/song-db-mock')
+const { SongDbMock } = require('../lib/song-db-mock')
 
 chai.use(chaiString)
 const { expect } = chai
 
+const songDbMock = new SongDbMock()
 const playlist = new Playlist(songDbMock)
 
 describe('Playlist Service', () => {
@@ -27,5 +28,12 @@ describe('Playlist Service', () => {
     const firstLetter = songs.map(({ title }) => _.first(title))
     expect(firstLetter[1]).to.equalIgnoreCase(lastLetters[0])
     expect(firstLetter[2]).to.equalIgnoreCase(lastLetters[1])
+  })
+
+  it('should pick fallback to next last letter if song not found', async () => {
+    songDbMock._setMode('withNotFoundLetters')
+    const songs = playlist.generate(3)
+    const titles = songs.map(({ title }) => title)
+    expect(titles).to.deep.equal(['YEAH RIGHT!!!', 'Toxic Poison', 'Chop Suey'])
   })
 })
