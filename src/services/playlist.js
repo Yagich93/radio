c5ce80a2a634f8d0fe9b276b224b6a1bf07037fe
Iakov Lushin <iakov.lushin@fulldive.com>
2019-12-07 16:36:15 +0300

    Add fallbacking to next last letter

--- src/services/playlist.js
+++ src/services/playlist.js
@@ -14,9 +14,17 @@ class Playlist {
 
   _appendNextSong(songs) {
     const lastSong = _.last(songs)
-    const lastLetter = _.last(lastSong.title)
-    const nextSong = this.songDb.sample({ firstLetter: lastLetter })
+    const nextSong = this._getNextSongByTitle(lastSong.title)
     songs.push(nextSong)
+  }
+
+  _getNextSongByTitle(title) {
+    const lastLetter = _.last(title)
+    const nextSong = this.songDb.sample({ firstLetter: lastLetter })
+    if (!nextSong) {
+      return this._getNextSongByTitle(title.slice(0, -1))
+    }
+    return nextSong
   }
 }
 
