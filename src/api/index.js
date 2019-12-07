abfdcfaeadd29e1b56f56dd33c5b3d5ed0b2585b
Iakov Lushin <iakov.lushin@fulldive.com>
2019-12-07 11:51:08 +0300

    Add starting koa

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,18 @@
+const Koa = require('koa')
+
 class Api {
-  listen() {
-    console.log('Server listening on port 8080')
+  constructor() {
+    // Setup koa
+    this.koa = new Koa()
+  }
+
+  listen(options = {}) {
+    const defaults = { port: 8080 }
+    const { port } = { ...options, ...defaults }
+
+    // Start http instance
+    this.koa.listen(port)
+    console.log('Server listening on port', port)
   }
 }
 
