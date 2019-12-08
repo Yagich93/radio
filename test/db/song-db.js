513c5092334a96da42305ac3ad78a44320365420
Iakov Lushin <iakov.lushin@fulldive.com>
2019-12-08 13:31:40 +0300

    Add test of sampling by first letter

--- test/db/song-db.js
+++ test/db/song-db.js
@@ -4,6 +4,7 @@ const _ = require('lodash')
 const { SongDb } = require('../../src/db/song-db')
 const testDbJson = require('../lib/test-db.json')
 const { TEST_SONGS } = require('../lib/test-songs')
+const B_SONG = TEST_SONGS.find(({ title }) => title.startsWith('B'))
 
 const songDb = new SongDb(testDbJson)
 
@@ -17,5 +18,10 @@ describe('Song DB', () => {
     const songs = _.times(20, () => songDb.sample())
     const deduplicatedSongs = _.uniq(songs)
     expect(deduplicatedSongs).to.have.length.greaterThan(1)
+  })
+
+  it('should sample by first letter', async () => {
+    const song = songDb.sample({ firstLetter: 'B' })
+    expect(song).to.deep.equal(B_SONG)
   })
 })
