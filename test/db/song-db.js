1427f3b23117378d2040025af864059d88f48edb
Iakov Lushin <iakov.lushin@fulldive.com>
2019-12-08 13:25:43 +0300

    Add test of sampling different songs

--- test/db/song-db.js
+++ test/db/song-db.js
@@ -1,7 +1,8 @@
 const { expect } = require('chai')
+const _ = require('lodash')
+
 const { SongDb } = require('../../src/db/song-db')
 const testDbJson = require('../lib/test-db.json')
-
 const { TEST_SONGS } = require('../lib/test-songs')
 
 const songDb = new SongDb(testDbJson)
@@ -10,5 +11,11 @@ describe('Song DB', () => {
   it('should sample random song without arguments', async () => {
     const song = songDb.sample()
     expect(TEST_SONGS).to.deep.include.members([song])
+  })
+
+  it('should sample different songs', async () => {
+    const songs = _.times(20, () => songDb.sample())
+    const deduplicatedSongs = _.uniq(songs)
+    expect(deduplicatedSongs).to.have.length.greaterThan(1)
   })
 })
