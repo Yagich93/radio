77b536fd80d935b15edccdf30807ab25d19a43d3
Iakov Lushin <iakov.lushin@fulldive.com>
2019-12-08 11:54:54 +0300

    Add parsing db json

--- src/db/song-db.js
+++ src/db/song-db.js
@@ -1,5 +1,23 @@
 class SongDb {
+  constructor(dbJson) {
+    this.songs = this._parseDbJson(dbJson)
+  }
+
   sample() {}
+
+  _parseDbJson(dbJson) {
+    const songs = []
+    dbJson.Library.Artist.map(artistJson => {
+      const artistSongs = artistJson.Song.map(({ _name, _id, _duration }) => ({
+        artist: artistJson._name,
+        durationMs: _duration,
+        id: _id,
+        title: _name
+      }))
+      songs.push(...artistSongs)
+    })
+    return songs
+  }
 }
 
 module.exports = { SongDb }
