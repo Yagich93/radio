32d4a369a3155753566e17ed4e86818c6c3139f1
Iakov Lushin <iakov.lushin@fulldive.com>
2019-12-08 13:18:51 +0300

    Add parsed test songs

---
+++ test/lib/test-songs.js
@@ -0,0 +1,34 @@
+const TEST_SONGS = [
+  {
+    artist: '10,000 Maniacs',
+    durationMs: '206706',
+    id: '3276',
+    title: 'Peace Train'
+  },
+  {
+    artist: '10,000 Maniacs',
+    durationMs: '199575',
+    id: '3332',
+    title: 'Candy Everybody Wants (unplugged)'
+  },
+  {
+    artist: '10,000 Maniacs',
+    durationMs: '250383',
+    id: '3277',
+    title: 'Eden'
+  },
+  {
+    artist: '10,000 Maniacs',
+    durationMs: '223399',
+    id: '3289',
+    title: 'Because The Night'
+  },
+  {
+    artist: '2PAC',
+    durationMs: '269244',
+    id: '4340',
+    title: 'Changes'
+  }
+]
+
+module.exports = { TEST_SONGS }
