a8acc7d8f4518658f87f7b0022cb47d69e7b2657
Iakov Lushin <iakov.lushin@fulldive.com>
2019-12-08 13:11:49 +0300

    Add starting up app with all layers

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const { Api } = require('./src/api')
 const { Playlist } = require('./src/services/playlist')
+const { SongDb } = require('./src/db/song-db')
+const dbJson = require('./assets/db.json')
 
-const playlist = new Playlist()
+const songDb = new SongDb(dbJson)
+const playlist = new Playlist(songDb)
 const api = new Api(playlist)
+
 api.listen()
